559c2f557247c28012c0f3ffe679ba3fe80e9891
AjayWagheDeveloper <wagheajay.developer@gmail.com>
2018-08-19 00:05:35 +0530

    ui commit

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,15 +1,23 @@
 import 'package:flutter/material.dart';
-class Home extends StatelessWidget{
+
+class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-   return new Scaffold(
-     appBar: new AppBar(
-       title:new Text("Hello Flutter"),
-       backgroundColor: Colors.green,
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Hello Flutter"),
+        backgroundColor: Colors.blue,
+      ),
+      body: new Container(
+        width: 420.0,
+        height: 210.0,
+        color: Colors.pink,
+        padding: EdgeInsets.all(75.0),
+        child: new Text("Welcome Ajay!",
+            textAlign: TextAlign.center,
+            style: new TextStyle(fontSize: 30.0, fontWeight: FontWeight.w800)),
+      ),
       
-     ),
-    
-   );
+    );
   }
-
 }
