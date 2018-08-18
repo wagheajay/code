0fe0f82febac6219cf3806c95cb8f6f333b0182e
AjayWagheDeveloper <wagheajay.developer@gmail.com>
2018-08-18 23:22:46 +0530

    third commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import './ui/home.dart';
 void main(){
 
   runApp(
     new MaterialApp(
       title:"Hello",
+      home:new Home()
 
     )
   );
---
+++ lib/ui/home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+class Home extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+   return new Scaffold(
+     appBar: new AppBar(
+       title:new Text("Hello Flutter"),
+       backgroundColor: Colors.green,
+      
+     ),
+    
+   );
+  }
+
+}
